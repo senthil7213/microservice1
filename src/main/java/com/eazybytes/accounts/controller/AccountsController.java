b026fc0b5955699f90a16ba9c763f3e46673adc7
senthil <senthil7213@gmail.com>
2026-01-11 22:57:26 +0530

    added validation

--- src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -4,34 +4,40 @@ import com.eazybytes.accounts.constant.AccountsConstants;
 import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ResponseDto;
 import com.eazybytes.accounts.service.IAccountsService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
+@Validated
 public class AccountsController {
     private IAccountsService iAccountsService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         iAccountsService.creatAccount(customerDto);
         return ResponseEntity.status(HttpStatus.CREATED).
                 body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
     @GetMapping("/fetch")
-    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) {
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
+                                                           @Pattern(regexp = "^[0-9]{10}$", message = "mobile number must be 10 digits")
+                                                           String mobileNumber) {
         CustomerDto customerDto = iAccountsService.fetchAccountDetails(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
 
     }
 
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto> updateAccountsDetails(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> updateAccountsDetails(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = iAccountsService.updateAccount(customerDto);
         if (isUpdated) {
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
@@ -42,7 +48,8 @@ public class AccountsController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam String mobileNumber) {
+    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam @Pattern(regexp = "^[0-9]{10}$", message = "mobile number must be 10 digits")
+                                                            String mobileNumber) {
         boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
         if (isDeleted) {
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
--- src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
+++ src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -1,11 +1,20 @@
 package com.eazybytes.accounts.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
+    @NotEmpty(message = "name can not be empty")
+    @Size(min = 5, max = 30, message = "The length of the customer name should be between 5 and 30")
     private String name;
+    @NotEmpty(message = "email can not be empty")
+    @Email(message = "email address should be valid value")
     private String email;
+    @Pattern(regexp = "^[0-9]{10}$", message = "mobile number must be 10 digits")
     private String mobileNumber;
     private AccountsDto accountsDto;
 }
